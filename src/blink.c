f1e0229bfdcd2c7c33eff264c642c7b29086ac63
David Risch <acc_mid@rischnet.de>
2020-04-15 17:49:00 +0200

    Added gpio_util

--- src/blink.c
+++ src/blink.c
@@ -1,10 +1,9 @@
-// From: https://github.com/pfalcon/esp-open-sdk
-
-
 #include "ets_sys.h"
 #include "gpio.h"
 #include "os_type.h"
 #include "osapi.h"
+
+#include "gpio_util.h"
 
 static const int pin = 4;
 static os_timer_t some_timer;
@@ -12,19 +11,14 @@ static os_timer_t some_timer;
 void some_timerfunc(void *arg) {
     os_printf("Timer triggered\r\n");
 
-    if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & (1 << pin)) {
-        GPIO_REG_WRITE(GPIO_OUT_ADDRESS, GPIO_REG_READ(GPIO_OUT_ADDRESS) & ~((1 << pin)));
-    } else {
-        GPIO_REG_WRITE(GPIO_OUT_ADDRESS, GPIO_REG_READ(GPIO_OUT_ADDRESS) | (1 << pin));
-    }
+    pin_set_value(pin, !pin_get_current_value(pin));
 }
 
 
 void blink_init() {
 
     gpio_init();
-
-    gpio_output_set(0, 0, (1 << pin), 0);
+    pin_set_output(pin);
 
     os_timer_setfn(&some_timer, (os_timer_func_t *) some_timerfunc, NULL);
     os_timer_arm(&some_timer, 750, 1);
