cea322989e4905fb9b53adf12166c5bba81907df
David Risch <acc_mid@rischnet.de>
2020-04-16 13:14:11 +0200

    Refactored uart

--- src/uart.c
+++ src/uart.c
@@ -9,88 +9,61 @@
 #include "gpio_util.h"
 #include "pins.h"
 
+// baudrate for sending and receiving
 #define UART_BAUDRATE 500
+
+// number of samples for each bit
 #define SENSE_FACTOR 5
-#define PACKET_LENGTH 8
 
 #define COUNTER_WAITING -999
 
 // every point (counter%SENSE_FACTOR)==0 is the edge between two bits
 int counter = COUNTER_WAITING;
-bool last_value = 1; // used to detect edges
 
-char values[1000];
-int raw_counter = COUNTER_WAITING;
-char raw_values[SENSE_FACTOR * 20];
-char bin_value = 0;
-int since_last = 0;
+// cycles since the end of the last word
+int since_last_word = 0;
+
+// used to improve stability against single flipped bits
+bool last_value = 1;
+
+// value of the current word
+char binary_word = 0;
+
 
 void timer_function(void *arg) {
-    //os_printf("Uart timer triggered\r\n");
+    bool current_value = pin_read_value(PIN_UART_IN);
 
-    bool value = pin_read_value(PIN_UART_IN);
-    //os_printf("v%d c%d   ", value, counter);
-
-    if (counter == COUNTER_WAITING && (!value && !last_value)) { // detect start bit (low)
-
-        pin_set_value(PIN_LED, !pin_get_current_value(PIN_LED));
-        bin_value = 0x00;
-        raw_values[raw_counter] = 0;
-        //os_printf("s_last %d  %s\r\n", since_last, raw_values);
-
-        counter = 0;
-
-        raw_values[raw_counter++] = '\0';
-        if (since_last > 10)
+    if (counter == COUNTER_WAITING && (!current_value && !last_value)) { // detect start bit (low)
+        // check if this is the beginning of a new transmission
+        if (since_last_word > 10) {
             os_printf("\r\n---------------------------------\r\n");
-        //os_printf("raw: %s   %d\r\n", raw_values, since_last);
-        raw_counter = 0;
-        since_last = 0;
-
-        //os_printf("---------------------------------\r\n");
-    } else if (counter == COUNTER_WAITING) {
-        since_last++;
-    }
-
-    if (counter >= 0 || (counter == COUNTER_WAITING && since_last < 50)) {
-        if (counter > 0 && counter % SENSE_FACTOR == 0) {
-            raw_values[raw_counter++] = ' ';
-            /*
-            if ((counter/SENSE_FACTOR)%10==0) {
-                raw_values[raw_counter] = 0;
-                os_printf("raw: %s\r\n", raw_values);
-                raw_counter = 0;
-            }
-            */
         }
 
-        raw_values[raw_counter++] = value ? '#' : ':';
-
+        binary_word = 0x00;
+        counter = 0;
+        since_last_word = 0;
+    } else if (counter == COUNTER_WAITING) {
+        since_last_word++;
     }
 
+    // check for the center of a bit
     if (counter >= 0 && counter % SENSE_FACTOR == SENSE_FACTOR / 2) {
         int position = counter / SENSE_FACTOR;
         // position 0: start bit (low)
         // position 1-8: data bit (low/high)
         // position 9: stop bit (high)
 
-        values[position] = '0' + value;
-
-        if (value && position >= 1 && position <= 8) {
-            bin_value |= (1 << (position - 1)); // input is LSB(1) to MSB(8)
-        }
-
-        if (position == 9) {
-            values[10] = '\0';
-            os_printf("%c", bin_value);
-            //os_printf("stop: %d %d  %c  %s \r\n", counter, raw_counter, bin_value, values);
-            raw_values[raw_counter++] = '~';
-            //os_printf("%s\r\n", raw_values);
+        if (current_value && position >= 1 && position <= 8) {
+            // data bit
+            binary_word |= (1 << (position - 1)); // input is LSB(1) to MSB(8)
+        } else if (position == 9) {
+            // stop bit
+            os_printf("%c", binary_word);
             counter = COUNTER_WAITING;
         }
     }
 
-    last_value = value;
+    last_value = current_value;
     if (counter != COUNTER_WAITING) {
         counter++;
     }
@@ -98,19 +71,15 @@ void timer_function(void *arg) {
 
 
 void my_uart_init() {
-    os_printf("Init start %d\r\n", 1000000 / UART_BAUDRATE / SENSE_FACTOR);
+    // try to get SENSE_FACTOR number of timer calls for each bit
+    int timer_interval = 1000000 / UART_BAUDRATE / SENSE_FACTOR;
+    os_printf("my_uart_init, interval for timer: %dus\r\n", timer_interval);
 
     pin_set_input(PIN_UART_IN);
 
-    pin_set_output(PIN_LED);
-
-    values[10] = '\0';
-
     hw_timer_init(NMI_SOURCE, 1);
-    hw_timer_arm(1000000 / UART_BAUDRATE / SENSE_FACTOR + 1);
+    hw_timer_arm(timer_interval);
     hw_timer_set_func((void (*)(void)) timer_function);
-
-    os_printf("Init end\r\n");
 }
 
 
